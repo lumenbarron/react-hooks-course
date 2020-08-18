0867fdee2d127165126144683a7e4c011016ae22
lumenbarron <lucemendez@gmail.com>
2020-08-18 13:39:04 -0500

    adding crud with firestore method delete, fix add

--- src/components/GetDataFirestore.js
+++ src/components/GetDataFirestore.js
@@ -3,15 +3,8 @@ import { firebase } from "../firebase";
 
 const GetDataFirestore = () => {
   const [data, setData] = useState([]);
-  const [tarea , setTarea] = useState('');
-  const [fecha, setFecha] = useState('');
-
-//   firebase.firestore().collection('tareas').add({
-//       tarea : 'segunda tarea',
-//       fecha:'hoy'
-//   }).then(ref => {
-//       console.log('added new task', ref.id);
-//   })
+  const [tarea, setTarea] = useState("");
+  const [fecha, setFecha] = useState("");
 
   useEffect(() => {
     console.log("useEfect");
@@ -19,61 +12,51 @@ const GetDataFirestore = () => {
   }, []);
 
   const getData = () => {
-      //added variable unsubscribe
-    const unsubscribe = firebase
-    firebase.firestore().collection('tareas')
-    .onSnapshot(snapshot => {
-        const arrayData = snapshot.docs.map(doc => ({
-            id: doc.id,
-            ...doc.data(),
-        }))
+    const unsubscribe = firebase;
+    firebase
+      .firestore()
+      .collection("tareas")
+      .onSnapshot((snapshot) => {
+        const arrayData = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
         setData(arrayData);
-    })
-    return () => unsubscribe()
-    
+      });
+    return () => unsubscribe();
   };
 
-//   const getData = async () => {
-//     const db = firebase.firestore();
-//     try {
-//       const data = await db.collection("tareas").get();
-//       const arrayData = data.docs.map((doc) => ({
-//         id: doc.id,
-//         ...doc.data(),
-//       }));
-//       console.log(arrayData);
-//       setData(arrayData);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
+  const deleteData = (id) => {
+      console.log('eliminado');
+      firebase.firestore().collection('tareas').doc(id).delete()
+  }
 
-//   const addNewData = async () => {
-//     try {
-//         const db = firebase.firestore()
-//         const newData = {
-//             name: data,
-//             fecha: Date.now()
-//         }
-//         const data = await db.collection('tareas').add({ name: data,
-//           fecha: Date.now()})
-  
-//           setData([...data, {id: data.id, ...newData}])
-//           setData('')
-//     } catch (error) {
-//         console.log(error);
-//     }
-//   }
-  
+  //   const getData = async () => {
+  //     const db = firebase.firestore();
+  //     try {
+  //       const data = await db.collection("tareas").get();
+  //       const arrayData = data.docs.map((doc) => ({
+  //         id: doc.id,
+  //         ...doc.data(),
+  //       }));
+  //       console.log(arrayData);
+  //       setData(arrayData);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
 
   const addData = (e) => {
-      e.preventDefault();
-      firebase.firestore().collection('tareas').add({
-        tarea ,
-        fecha
+    e.preventDefault();
+    firebase
+      .firestore()
+      .collection("tareas")
+      .add({
+        tarea,
+        fecha,
       })
-      .then(() => setTarea(''), setFecha(''))
-  }
+      .then(() => setTarea(""), setFecha(""));
+  };
 
   return (
     <Fragment>
@@ -82,14 +65,12 @@ const GetDataFirestore = () => {
           <div className="col-md-6">
             <h3>Lista de Tareas</h3>
             <ul className="list-group">
-            <div>
-
-    </div>
+              <div></div>
               {data.map((item) => (
                 <li className="list-group-item" key={item.id}>
                   <span>{item.tarea}</span>
                   <span>{item.fecha}</span>
-                  <button className="btn btn-danger btn-sm float-right">
+                  <button onClick={() => deleteData(item.id)} className="btn btn-danger btn-sm float-right">
                     Eliminar
                   </button>
                   <button className="btn btn-warning btn-sm float-right mr-2">
@@ -103,11 +84,20 @@ const GetDataFirestore = () => {
         </div>
       </div>
       <form onSubmit={addData}>
-      <input name="tarea" type="text" value={tarea} onChange={e => setTarea(e.currentTarget.value)} />
-      <input name="fecha" type="text" value={fecha} onChange={e => setFecha(e.currentTarget.value)} />
-      <button>Submit</button>
-    </form>
-
+        <input
+          name="tarea"
+          type="text"
+          value={tarea}
+          onChange={(e) => setTarea(e.currentTarget.value)}
+        />
+        <input
+          name="fecha"
+          type="text"
+          value={fecha}
+          onChange={(e) => setFecha(e.currentTarget.value)}
+        />
+        <button>Submit</button>
+      </form>
     </Fragment>
   );
 };
